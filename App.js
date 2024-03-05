3f02064a731474c036d6b5c731adfe4b3ca4b358
Summy Singh <summysingh86@gmail.com>
2024-03-05 20:11:35 +0530

    chapter 3
    lesson and assignment about JSX

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-//Way 1
 
-/*
-const heading = React.createElement("h1",{}, "Hello React Heading 1");
+ const Header = ()=>{
+    
+ }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-*/
-
-//way 2
-const heading = React.createElement("h1",{}, "Hello React Heading 1");
-const heading2 = React.createElement("h2",{}, "Hello React Heading 2 from Parcel");
-
-const container = React.createElement("div",{
-    id:"container",
-    className:"container"
-}, [heading, heading2]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
-
+root.render(<Header/>)
